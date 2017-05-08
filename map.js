2652b3d553195e2700a1f26eb7a63214a804b269
outdoorpet <acooper8@utas.edu.au>
2017-05-08 16:22:40 +1000

    minor fix

--- map.js
+++ map.js
@@ -18,14 +18,6 @@ var passiveIcon = L.divIcon({
     popupAnchor: L.point(0,-20)
 });
 
-var activeRefIcon = L.divIcon({
-    className: 'svg-marker',
-    html: '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="margin: 0 auto; width: 20px; height:20px;"><polygon style="fill:Red; stroke:#666666; stroke-width:2; stroke-opacity:0.5"points="0,0 20,0 10,20"/></svg>',
-    iconSize: L.point(20, 20),
-    iconAnchor: L.point(10, 20),
-    popupAnchor: L.point(0,-20)
-});
-
 var passiveRefIcon = L.divIcon({
     className: 'svg-marker',
     html: '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="margin: 0 auto; width: 20px; height:20px;"><polygon style="fill:Yellow; stroke:#666666; stroke-width:2; stroke-opacity:0.5"points="0,0 20,0 10,20"/></svg>',
